a19ab54d134402b2c78a8b7732914e6ccafc01b8
Igao2 <igor.dasilvacosta8@gmail.com>
2023-09-05 11:21:45 -0300

    Front changed, init map defined

--- app.py
+++ app.py
@@ -1,24 +1,17 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+   
     return render_template("index.html")
-
-dicionario = { 'A':{"B":5,"C":6},
-              'B':{"D":3},
-              'C':{"D":4, "E":2},
-              'D':{"E":1},
-              'E':{"E":0}}
 
 @app.route("/", methods=['POST'])
 def post():
-    valorA = request.form['valorA']
-    valorE = request.form['valorE']
-    
+   value = request.form["valueA"]
   
-    
+   
 if __name__ == "__main__":
     app.run()
 
